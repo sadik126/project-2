8e82ef0abdc42877381fae6a9d0629928cb4ac00
sadik126 <sadikhimel04@gmail.com>
2021-10-10 09:20:42 +0600

    commit

---
+++ index.js
@@ -0,0 +1,91 @@
+console.log("hello sadik")
+
+
+//constructor : create book object
+
+function book(name, author, type) {
+
+    this.name = name;
+    this.author = author;
+    this.type = type;
+
+}
+
+//display constructor
+
+function display() {
+
+}
+
+display.prototype.add = function(book){
+    console.log("adding to form");
+    tablebody = document.getElementById('tablebody');
+    let uistring = `
+ <tr>
+    
+    <td>${book.name}</td>
+    <td>${book.author}</td>
+    <td>${book.type}</td>
+</tr>`;
+tablebody.innerHTML += uistring;
+}
+
+display.prototype.clear = function(){
+    let libraryform = document.getElementById("libraryform");
+    libraryform.reset();
+
+}
+
+//add methods to display protoypes
+
+
+
+
+//add submit event listener
+let libraryform = document.getElementById("libraryform");
+libraryform.addEventListener('submit', libraryformsubmit);
+
+function libraryformsubmit(e) {
+    console.log("welcome to form submuit");
+    let name = document.getElementById("name").value;
+    let author = document.getElementById("author").value;
+    let type;
+
+
+    let fiction = document.getElementById("fiction");
+    let programming = document.getElementById("programming");
+    let horror = document.getElementById("horror");
+
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (horror.checked) {
+        type = horror.value;
+
+    }
+    // create object book
+    let Book = new book(name, author, type);
+    console.log(Book);
+
+    let Display = new display();
+    Display.add(book); 
+    Display.clear();
+    e.preventDefault();
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
